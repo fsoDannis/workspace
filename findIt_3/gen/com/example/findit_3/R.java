16b427ff6ce14f8a08300250a9a929fcbc2c4bb3
Dan <Dan@Dinkydetails.com>
2013-01-31 22:42:50 -0500

    Creating new app to try XML

--- findIt_3/gen/com/example/findit_3/R.java
+++ findIt_3/gen/com/example/findit_3/R.java
@@ -12,13 +12,30 @@ public final class R {
     }
     public static final class drawable {
         public static final int background=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int background2=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int _address=0x7f070008;
+        public static final int _distance=0x7f07000d;
+        public static final int _name=0x7f070007;
+        public static final int _phone=0x7f07000b;
+        public static final int addresssLabel=0x7f070009;
+        public static final int b_Enter=0x7f070002;
+        public static final int button1=0x7f070006;
+        public static final int distanceLabel=0x7f07000c;
+        public static final int editText1=0x7f07000e;
+        public static final int et_EnterName=0x7f070003;
+        public static final int linearLayout1=0x7f070001;
+        public static final int linearLayout2=0x7f070000;
+        public static final int menu_settings=0x7f07000f;
+        public static final int nameLabel=0x7f070005;
+        public static final int phoneLabel=0x7f07000a;
+        public static final int textView1=0x7f070004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int second=0x7f030001;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
--- findIt_3/src/com/example/findit_3/Main.java
+++ findIt_3/src/com/example/findit_3/Main.java
@@ -1,8 +1,16 @@
 package com.example.findit_3;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class Main extends Activity {
 
@@ -10,7 +18,37 @@ public class Main extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
+		
+		Button B = (Button) findViewById(R.id.b_Enter);
+		B.setBackgroundColor(Color.WHITE);
+		B.setTextColor(Color.BLACK);
+		
+		final EditText et = (EditText) findViewById(R.id.et_EnterName);
+		et.setTextColor(Color.WHITE);
+		//ImageView iv = (ImageView) findViewById(R.drawable.newbg);
+	
+		
+		/*  Getting Image from the Gallery
+		ImageView iv = (ImageView) findViewById(R.id.imageView1);
+		iv.setImageURI ( (Uri) getIntent().getExtras().get(Intent.EXTRA_STREAM));
+		*/
+		
+		
+		//Sending the information from the Edit Box through the button and onto the next screen
+		B.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// Sending from this class to the "second" view/class
+				Intent intent = new Intent(Main.this, Second_Activity.class);
+				//This is the information that will be sent.
+				intent.putExtra("Entered Text", "Hello, " + et.getText().toString());
+				startActivity(intent);
+			}
+		});
+		
+	
+}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
